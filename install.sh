a59b55d4fa0f55b3e224b21875ab054cf2d8cbfa
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-04-23 02:24:44 -0700

    Add a simple install script
    Create symlinks from /usr/local/bin/ to the cw-* binaries

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+UID=$(id -u)
+INSTALL_SH=$(readlink -f $0)
+INSTALL_DIR=$(dirname $INSTALL_SH)
+DEST="/usr/local/bin"
+BINARIES="cw-clearconsole cw-info cw-mux cw-swpower"
+
+if [ $UID -ne 0 ]
+then
+  echo "You must run this script as root" >&2
+  exit 1
+fi
+
+if [ ! -e $DEST ]
+then
+  mkdir -p $DEST
+  chmod 0755 $DEST
+fi
+
+for BIN in $BINARIES
+do
+  ln -sf $INSTALL_DIR/$BIN $DEST/$BIN
+  chmod 0755 $DEST/$BIN
+done
+
+exit 0
